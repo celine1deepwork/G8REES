40bc2b265c2d627e79eae546a4a9e37c2df25e7e
celine ozdemir <69770393+celine1deepwork@users.noreply.github.com>
2023-12-08 17:14:08 +0300

    Yapılanlar / Yapılacaklar
    Yapılanlar :
    Approve Sayfası Yapıldı
    a) Reject / Accept Butonları eklendi
    b) Fonksiyonları Yapıldı
    c) Accept Reject yapısına göre Count işlemi yapıldı.
    d) Notification eklendi
    
    Admin Home :
    a)Show More Details / Less Details yapıldı
    b) Notification Eklendi.
    c) Logout Bağlandı.
    d) Notification Kartı silindi, yerine ring-bell eklendi.
    
    Admin Login - Admin sayfasına bağlandı
    
    Home, Buy, Sell, Agent Finder Sayfalarında Log-out Sign-in'e bağlandı.
    
    Notification bell düzeltildi Home Page 'de

--- HomePage/src/js/approvals.js
+++ HomePage/src/js/approvals.js
@@ -1,30 +1,61 @@
 
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+let acceptedCount = 0;
+let rejectedCount = 0;
 
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
-});
+function updateApprovalCounts() {
+  document.getElementById('accepted-count').textContent = acceptedCount;
+  document.getElementById('rejected-count').textContent = rejectedCount;
+}
+
+// Function to handle acceptance
+function handleAcceptance() {
+  acceptedCount++;
+  updateApprovalCounts();
+}
+
+// Function to handle rejection
+function handleRejection() {
+  rejectedCount++;
+  updateApprovalCounts();
+}
+
+//Get's the Notification style element and call's the notification-success-box used to show it on screen by timeout 
+function showNotificationSuccess(message) {
+  const notificationBox = document.createElement('div');
+  notificationBox.classList.add('notification-success-box');
+  notificationBox.textContent = message;
+
+  document.body.appendChild(notificationBox);
+  handleAcceptance();
+  setTimeout(() => {
+    document.body.removeChild(notificationBox);
+  }, 5000);
+}
+//Get's the Notification style element and call's the notification-reject-box used to show it on screen by timeout
+function showNotificationReject(message) {
+  const notificationBox = document.createElement('div');
+  notificationBox.classList.add('notification-reject-box');
+  notificationBox.textContent = message;
+
+  document.body.appendChild(notificationBox);
+
+  setTimeout(() => {
+    document.body.removeChild(notificationBox);
+  }, 5000);
+}
+//Removes the .card element by confirmation
+function removeCard(button) {
+  const card = button.closest('.card');
+  if (card) {
+    card.remove();
+  }
+}
+//Reject Confirmation
+function confirmReject(button) {
+  const isConfirmed = window.confirm('Are you sure you want to reject this Estate?');
+  if (isConfirmed) {
+    removeCard(button);
+    showNotificationReject('Estate has been rejected.');
+    handleRejection();
+  }
+}
